ab54ab31a87c7628077ebabfb88e083b16f124be
LouisePaulDelvaux <louise.paul.delvaux@gmail.com>
2015-06-17 18:41:23 +0200

    Describe data

--- eic/data_handler_eic.py
+++ eic/data_handler_eic.py
@@ -3,18 +3,23 @@
 Author: LPaul-Delvaux
 Created on 18 may 2015
 '''
-from format_careers import format_career_tables, aggregate_career_table
+from format_careers import format_career_tables, aggregate_career_table, final_career_table
 from format_individual_info import format_individual_info
 from load_data import load_data_eic
 from select_data import select_data
 
 
-def format_data(data):
+def format_data(data, describe=False):
     ''' Format datasets '''
     careers = format_career_tables(data)
     careers_formated = aggregate_career_table(careers)
+    career_table = final_career_table(careers_formated)
     individual_info_formated = format_individual_info(data)
-    data_formated = {'careers': careers_formated, 'individus': individual_info_formated}
+    data_formated = {'careers': career_table, 'individus': individual_info_formated}
+    if describe:
+        from stat_describe_eic import describe_individual_info, describe_missing
+        describe_individual_info(individual_info_formated)
+        describe_missing(data_formated, 'sal_brut_deplaf')
     return data_formated
 
 
@@ -24,7 +29,7 @@ def import_data(path_data, path_storage, datasets_to_import, file_description_pa
                              file_description_path, datasets_to_import, test=True)
     data = select_data(data_raw, file_description_path,
                        first_year = 1952, last_year = 2009)
-    data = format_data(data)
+    data = format_data(data, describe=True)
     return data
 
 
@@ -41,6 +46,5 @@ if __name__ == '__main__':
     file_description_path = path_storage + config.get('EIC', 'file_description_name')
     datasets_to_import = ["b100_09", "b200_09", "c200_09", "c100_09", "dads_09", "pe200_09", "etat_09"]
     data = import_data(path_data, path_storage, datasets_to_import, file_description_path)
-
     t1 = time.time()
     print '\n Time for importing data {}s.'.format('%.2f' % (t1 - t0))
