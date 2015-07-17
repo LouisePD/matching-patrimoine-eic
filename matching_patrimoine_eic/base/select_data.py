a786b9f9431ff9f1b844ed70c6a96d361183e914
LouisePaulDelvaux <louise.paul.delvaux@gmail.com>
2015-07-17 18:34:12 +0200

    Improved selection

--- matching_patrimoine_eic/base/select_data.py
+++ matching_patrimoine_eic/base/select_data.py
@@ -31,20 +31,45 @@ def codes_regimes_to_import(file_description):
 
 
 def select_complete_career(data, target_var=None, time_var=None, id_var='noind',
-                           thresholds = {'nb_years_min': 30, 'missing_wages': 0.05}):
+                           thresholds = {'nb_years_min': 30, 'missing_wages': 0.10}):
     ''' This function selects individuals with missing work employment status Throughout their career below a threshold
     Note: As selection is based on assumed workstate definitionsg, it can not be included as a step of select_data'''
-    df = data['careers'].copy()
-    nb_time_by_indiv = df[[id_var, time_var]].groupby([id_var])[time_var].count()
-    df['missing'] = df[target_var].isnull()
-    pct_missing = df[[id_var, 'missing']].groupby([id_var])['missing'].sum() / nb_time_by_indiv
-    to_keep = (nb_time_by_indiv >= thresholds['nb_years_min']) * (pct_missing <= thresholds['missing_wages'])
-    indiv_to_keep = list(set(to_keep[to_keep == True].index))
+    df = data['careers']
+    assert sum(df[time_var].isnull()) == 0
+    assert (df['time_unit'] == time_var).all()
+    assert sum(df.duplicated([time_var, id_var])) == 0
+    nb_time_by_indiv = df.groupby([id_var], sort = True)[time_var].count()
+    min_by_indiv = df.groupby([id_var], sort = True)[time_var].min()
+    max_by_indiv = df.groupby([id_var], sort = True)[time_var].max()
+    range_years = max_by_indiv - min_by_indiv + 1
+    assert (nb_time_by_indiv <= range_years).all()
+    df['missing'] = (df[target_var].isnull()).astype(int)
+    nb_unknown_years = df.groupby([id_var], sort = True)['missing'].sum()
+    pct_missing = 1 - nb_time_by_indiv / range_years
+    to_keep = (range_years >= thresholds['nb_years_min']) * (pct_missing <= thresholds['missing_wages'])
+    indiv_to_keep = list(set(to_keep[to_keep].index))
+    assert len(indiv_to_keep) < len(to_keep)
+    try:
+        assert len(indiv_to_keep) != 0
+    except:
+        print 'Options of selection - see below might be too restrictive: \n'.format(thresholds)
+        print sum(range_years >= thresholds['nb_years_min'])
+        print sum(pct_missing <= thresholds['missing_wages'])
+        assert len(indiv_to_keep) != 0
+    data['individus'] = data['individus'].sort(['noind'])
+    if len(data['individus'].index) != len(nb_time_by_indiv.index):
+        should_not_exist = [idx for idx in data['individus'].index if idx not in nb_time_by_indiv.index]
+        print('People with individual information but no information on their career \n {}'.format(should_not_exist))
+    df = data['individus']
+    df.loc[df['noind'].isin(nb_time_by_indiv.index.values), 'nb_obs_career'] = nb_time_by_indiv.values.astype(int)
+    df.loc[df['noind'].isin(min_by_indiv.index.values), 'min_year_career'] = min_by_indiv.astype(int)
+    df.loc[df['noind'].isin(pct_missing.index.values), 'pct_missing'] = pct_missing.values
+    df.loc[df['noind'].isin(max_by_indiv.index.values), 'max_year_career'] = max_by_indiv.astype(int)
+    data['individus'] = df
     for table in data.keys():
         data[table] = data[table].loc[data[table]['noind'].isin(indiv_to_keep), :]
-    data['selection'] = pd.DataFrame({'nb_times': nb_time_by_indiv, 'missing': pct_missing,
-                                      'select': to_keep, 'noind': df['noind']})
-    data['individus']['nb_obs_career'] = nb_time_by_indiv
+        print table
+        assert data[table].shape[0] != 0
     return data
 
 
@@ -52,7 +77,7 @@ def select_generation(data, first_generation, last_generation):
     print "    Only generations between {} and {} have been selected".format(first_generation, last_generation)
     info_birth = data['individus'][['anaiss', 'noind']].copy()
     to_keep = (info_birth.anaiss >= first_generation) & (info_birth.anaiss <= last_generation)
-    ind_to_keep = set(info_birth.loc[to_keep, 'noind'])
+    ind_to_keep = list(set(info_birth.loc[to_keep, 'noind']))
     for table in data.keys():
         data[table] = data[table].loc[data[table]['noind'].isin(ind_to_keep), :]
     return data
@@ -62,12 +87,17 @@ def select_generation(data, first_generation, last_generation):
 def select_generation_before_format(first_generation, last_generation,
                                     reference_table, var_naiss, temporary_store = None):
     print "    Only generations between {} and {} have been selected".format(first_generation, last_generation)
+    if not first_generation:
+        first_generation = 1900
+    if not last_generation:
+        last_generation = 2050
     info_birth = temporary_store.select(reference_table, columns=[var_naiss, 'noind'])
     to_keep = (info_birth[var_naiss] >= first_generation) & (info_birth[var_naiss] <= last_generation)
-    ind_to_keep = set(info_birth.loc[to_keep, 'noind'])
-    for table in temporary_store.keys():
-        df = temporary_store.select(table, where='noind in ind_to_keep')
-        temporary_store.put(table, df, format='table', data_columns=True, min_itemsize = 20)
+    ind_to_keep = list(set(info_birth.loc[to_keep, 'noind']))
+    for table_name in temporary_store.keys():
+        df = temporary_store.select(table_name, where='noind=ind_to_keep')
+        temporary_store.remove(table_name)
+        temporary_store.put(table_name, df, format='table', data_columns=True, min_itemsize = 20)
 
 
 def select_regimes(table_career, code_regime_to_import_by_dataset):
